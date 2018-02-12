1d3e41d60fea035f295346883cc36e313208b1ec
CristianV <cristianvg2003@gmail.com>
2018-02-12 09:18:40 -0500

    refactor

--- tic-tack-toe/src/index.js
+++ tic-tack-toe/src/index.js
@@ -14,7 +14,7 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {squares: Array(9).fill(null),
-                        xIsNext: true
+                      xIsNext: true
                      };
     }
 
@@ -33,7 +33,7 @@ class Board extends React.Component {
             return;
         }
 
-        squares[i]    = this.state.xIsNext ? "X" : "O";
+        squares[i] = this.state.xIsNext ? "X" : "O";
         this.setState({squares: squares,
                        xIsNext: !this.state.xIsNext
         });
@@ -49,29 +49,37 @@ class Board extends React.Component {
         }
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-            </div>
-        </div>
         );
     }
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{squares: Array(9).fill(null)}],
+            xIsNext: true
+        };
+    }
+
     render() {
         return (
             <div className="game">
