c4ec071e42b22dccc5ff0b9ba216c3561eed0fb0
CristianV <cristianvg2003@gmail.com>
2018-03-13 23:03:12 -0500

    Adding base components

--- reservas/index.js
+++ reservas/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 
 ReactDOM.render(
- <h1>Hello World with JSX, Babel and Webapck</h1>,
+ <App/>,
  document.getElementById("root")
 );
