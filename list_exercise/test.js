2ceaa4a9349e5a98dff331d4cc271c222f56b8ee
CristianV <cristianvg2003@gmail.com>
2020-12-22 08:35:46 -0500

    working

--- list_exercise/test.js
+++
@@ -1,8 +0,0 @@
-function miTaggedTemplateLiteral(strings, ...values) {
-  return console.log(strings, ...values);
-}
-
-let nombre = "Carlos";
-let edad = 32;
-
-miTaggedTemplateLiteral`Hola soy ${nombre} y tengo ${edad} años`;
